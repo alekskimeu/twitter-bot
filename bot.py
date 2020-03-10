be5dd6a0d9b8855ae426feabc8dbe45f9a20ccd2
alexkimeu0 <kim3ual3x@gmail.com>
2020-03-10 16:24:16 +0300

    Deploying lexbot0

---
+++ bot.py
@@ -0,0 +1,37 @@
+import tweepy
+from time import sleep
+from credentials import *
+
+
+# Access & authorize Twitter credentials from credentials.py
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+# For loop to iterate over tweets with #freeCodeCamp, limit to 10
+terms = '#freeCodeCamp OR #100DaysofCode OR #CodeNewbie'
+for tweet in tweepy.Cursor(api.search,
+                           q=terms).items(10):
+    
+    # Print out usernames of the last 10 people to use #freeCodeCamp
+    try:
+        print('Tweet by: @' + tweet.user.screen_name)
+
+        tweet.retweet()
+        print('Retweeted the tweet')
+
+        tweet.favorite()
+        print("Favorited the tweet")
+
+        if not tweet.user.following:
+            tweet.user.follow()
+            print("Followed the user")
+        sleep(14400)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except:
+        StopIteration
+        break
+
