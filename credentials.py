9285e2d24468792eef48850aca498ae6b603d976
alexkimeu0 <61450115+alexkimeu0@users.noreply.github.com>
2020-03-25 12:44:54 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 # Create Variables for each key, secret & token.
-consumer_key = 'JZjWmq18Y5a0b5IG4S7OpDQzx'
-consumer_secret = 'shnkrjvQ8QRLK3XuhFb25xbUNeJFLXCFCG7ZkTns3IQvYkFrJi'
-access_token = '1236998873161351168-schFUkqSIDRmnKIYFsNgjfurTCNVdF'
-access_token_secret = 'M7xh2aUEp8jwHkaj50Ov5ExwygD5f1XPZpYvbexcBcXpI'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
